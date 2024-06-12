bf28f25fdce51fb62e1c1be2582476218b8ca3b3
Arseniy <140651932+fakelag16@users.noreply.github.com>
2024-06-12 16:15:45 +0200

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -7,8 +7,8 @@ int main() {
 	srand(time(0));
 	int tryLimit = 10;
 	int userNum;
-	int start = 0;
-	int end = 20;
+	int start = 1;
+	int end = 100;
 	int x = rand() % (end - start + 1) + start;
 	bool tryAgain = true;
 
@@ -16,25 +16,27 @@ int main() {
 
 	
 
-	while (tryAgain)
-	{
-		if (tryLimit != 0)
-		{
-			cout << "Введите число: ";
-			cin >> userNum;
+	while (tryAgain && tryLimit > 0) {
+		cout << "Осталось попыток: " << tryLimit << endl;
+		cout << "Введите число: ";
+		cin >> userNum;
 
-			if (userNum < x) {
-				cout << "Загаданное число больше." << endl;
-			}
-			else if (userNum > x) {
-				cout << "Загаданное число меньше." << endl;
-			}
-			else
-			{
-				cout << "Поздравляю! Вы угадали число " << x << " за " << tryLimit << " попыток." << endl;
-				tryAgain = false;
-			}
+		if (userNum < x) {
+			cout << "Загаданное число больше." << endl;
 		}
+		else if (userNum > x) {
+			cout << "Загаданное число меньше." << endl;
+		}
+		else
+		{
+			cout << "Поздравляю! Вы угадали число " << x << " за " << tryLimit << " попыток." << endl;
+			tryAgain = false;
+		}
+		tryLimit--;
+	}
+
+	if (tryLimit == 0 && tryAgain) {
+		cout << "К сожалению, вы исчерпали все попытки. Загаданное число было " << x << "." << endl;
 	}
 	cout << "Program ended";
 }
