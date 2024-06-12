21c7e138f1a6da70cd2c7064240093f77658822f
Arseniy <140651932+fakelag16@users.noreply.github.com>
2024-06-12 16:09:21 +0200

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -5,25 +5,35 @@ using namespace std;
 int main() {
 	setlocale(LC_ALL, "Ru");
 	srand(time(0));
+	int tryLimit = 10;
 	int userNum;
-	int start = 10;
+	int start = 0;
 	int end = 20;
 	int x = rand() % (end - start + 1) + start;
 	bool tryAgain = true;
 
+	cout << "Компьютер загадал число от 1 до 100. Попробуй угадать!" << endl;
+
+	
+
 	while (tryAgain)
 	{
-		cin >> userNum;
-		if (userNum < x) {
-			cout << "Загаданное число больше." << endl;
-		}
-		else if (userNum > x) {
-			cout << "Загаданное число меньше." << endl;
-		}
-		else
+		if (tryLimit != 0)
 		{
-			cout << "Вы угадали!" << endl;
-			tryAgain = false;
+			cout << "Введите число: ";
+			cin >> userNum;
+
+			if (userNum < x) {
+				cout << "Загаданное число больше." << endl;
+			}
+			else if (userNum > x) {
+				cout << "Загаданное число меньше." << endl;
+			}
+			else
+			{
+				cout << "Поздравляю! Вы угадали число " << x << " за " << tryLimit << " попыток." << endl;
+				tryAgain = false;
+			}
 		}
 	}
 	cout << "Program ended";
