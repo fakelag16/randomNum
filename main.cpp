3f1f1b8f8cd99e1987fee4b17e09360e93674324
Arseniy <140651932+fakelag16@users.noreply.github.com>
2024-06-12 15:52:22 +0200

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -2,30 +2,29 @@
 
 using namespace std;
 
-
 int main() {
-    setlocale(LC_ALL, "Ru");
-    srand(time(0));
-    int userNum;
-    int start = 10;
-    int end = 20;
-    int x = rand() % (end - start + 1) + start;
-    bool tryAgain = true;
+	setlocale(LC_ALL, "Ru");
+	srand(time(0));
+	int userNum;
+	int start = 10;
+	int end = 20;
+	int x = rand() % (end - start + 1) + start;
+	bool tryAgain = true;
 
-
-    while (tryAgain)
-    {
-        cin >> userNum;
-        if (x == userNum)
-        {
-            cout << "Вы угадали! ";
-            tryAgain = false;
-        }
-        else
-        {
-            cout << "Вы не угадали ";
-        }
-    }
-    cout << "Program ended";
-    
+	while (tryAgain)
+	{
+		cin >> userNum;
+		if (userNum < x) {
+			cout << "Загаданное число больше." << endl;
+		}
+		else if (userNum > x) {
+			cout << "Загаданное число меньше." << endl;
+		}
+		else
+		{
+			cout << "Вы угадали!" << endl;
+			tryAgain = false;
+		}
+	}
+	cout << "Program ended";
 }
